f06008b395561ec30797dcea6d6082741ea15b04
Harish Malik <harish.malik@asu.edu>
2017-04-30 23:53:42 -0700

    Added: contextMenu on selection to add protein.

--- ProteinTracker/eventPage.js
+++ ProteinTracker/eventPage.js
@@ -1,3 +1,34 @@
+var menuItem = {
+    "id" : "addProtein",
+    "title": "Add Protein",
+    "contexts": ["selection"]
+}
+
+chrome.contextMenus.create(menuItem);
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+   
+    if(clickData.menuItemId == "addProtein" && clickData.selectionText){
+         
+        var intRegex = /^\d+$/;
+        if(intRegex.test(clickData.selectionText)){
+            
+            chrome.storage.sync.get('total', function(items){
+                //alert(items.total);
+                var newTotal = 0;
+                if(items.total){
+                    newTotal += parseInt(items.total);
+                }
+                
+                
+                newTotal += parseInt(clickData.selectionText);
+                //alert(newTotal);
+                chrome.storage.sync.set({'total': newTotal});
+            });
+        }
+    }
+})
+
 chrome.storage.onChanged.addListener(function(changes){
     chrome.browserAction.setBadgeText({"text": changes.total.newValue.toString() });
 });
