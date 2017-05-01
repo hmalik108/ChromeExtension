c081981b93da75960e553e61b1b28d89755973c5
Harish Malik <harish.malik@asu.edu>
2017-04-30 22:52:08 -0700

    Added: Notifications for Protein Tracker

--- ProteinTracker/options.js
+++ ProteinTracker/options.js
@@ -14,7 +14,15 @@ $(function() {
     });
     
     $('#reset').click(function(){
-        chrome.storage.sync.set({'total':0});
+        chrome.storage.sync.set({'total':0}, function(){
+            var opt = {
+                type: "basic",
+                title:"Total reset!",
+                message:"Total has been reset back to 0.",
+                iconUrl:"icon.png"
+            }
+            chrome.notifications.create('reset', opt, function(){});
+        });
     });
     
   
